a8189357ec69a305154a9d6207cd859fd6418805
Mihaly Nyilas <dr.dabzse@gmail.com>
2023-02-19 14:49:13 +0100

    removed: unneded spaces

--- passwd_crypt.py
+++ passwd_crypt.py
@@ -1,13 +1,13 @@
 from cryptography.fernet import Fernet
- 
+
 message = "this will be encrypted, and decrypted"
 key = Fernet.generate_key()
 fernet = Fernet(key)
 encMessage = fernet.encrypt(message.encode())
- 
+
 print(f"original string: {message}")
 print(f"encrypted string: {encMessage}")
- 
+
 decMessage = fernet.decrypt(encMessage).decode()
- 
+
 print(f"decrypted string: {decMessage}")
